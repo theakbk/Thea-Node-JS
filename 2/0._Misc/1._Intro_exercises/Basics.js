13a2c448e9d68b0cd10b922bc7e581cb09da7160
Tkam <thea@kampmann.dk>
2021-02-04 18:17:26 +0100

    Indledende opgaver Node JS

---
+++ 2/0._Misc/1._Intro_exercises/Basics.js
@@ -0,0 +1,29 @@
+// --------------------------------------
+// Variables, strings, numbers, floats
+// --------------------------------------
+// Exercise 1 - Console and constants
+
+const firstName = "Thea";
+const lastName = "Kampmann";
+
+console.log("My firstname is " + firstName + " and my last name is " + lastName);
+
+// EXERCISE
+// show in the console
+// My first name is Anders and my last name is Latif
+
+
+// --------------------------------------
+// Exercise 2 - Numbers and Strings
+
+const year = "2020";
+const number = 1;
+
+// Add the year plus the number
+// The result should be 2021
+// You cannot touch line 1 or 2
+
+console.log(number + + year);
+
+
+// --------------------------------------
