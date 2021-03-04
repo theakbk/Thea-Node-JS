0cd8a2b43b14f23c1eca48a4526210f2e56b3ab1
Tkam <thea@kampmann.dk>
2021-03-04 11:07:54 +0100

    Rest API, update, delete, create

--- 2/1. First-server/app.js
+++ 2/1. First-server/app.js
@@ -1,6 +1,9 @@
 const express = require ("express")
 const app = express();
 
+app.use(express.json());
+
+
 app.get("/list", (req, res) => 
 {
     res.send(heste)
@@ -19,12 +22,56 @@ app.get("/get/:id", (req, res) =>
 });
 
 
-app.get("/delete/:id", (req, res) => 
+
+//Post
+app.post("/create", (req, res) => {
+
+    console.log(req.body)
+    console.log("id = " + req.body.id + " Name = " + req.body.name)
+
+    if (req.body.id in heste)
+    {
+        return res.send({status: "ID already exists"})
+    }
+    heste[req.body.id] = req.body
+    res.send({ status: "200 ok"})
+});
+
+
+
+app.delete("/delete/:id", (req, res) => 
 {
     delete heste[req.params.id]
     //res.send(heste[req.params.id])
     res.send(heste);
 });
+
+app.put("/update/:id", (req, res) => {
+    console.log(req.body)
+    console.log("id = " + req.params.id + " Name = " + req.body.name)
+
+    if (!(req.params.id in heste))
+    {
+        return res.send({status: "404 not found"})
+    }
+
+    //Opdater og overskriv hele Json. Med risiko for slette felter der ikke skal opdateres
+    //heste[req.params.id] = req.body
+
+
+    //Opdater enkelte felter, uden risiko for slettede felter
+    for (let m in req.body)
+    {
+        if (m != "id")
+        {
+          let hest=heste[req.params.id]
+          hest[m] = req.body[m]
+        }
+    }
+    
+    //heste[req.body.id] = req.body
+    res.send({ status: "200 ok"})
+})
 
 
 
